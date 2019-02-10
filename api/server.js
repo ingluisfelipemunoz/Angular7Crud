de4872c319481d09a47ab3b7258550f2ad9c9277
JarvisXxX <eldrizzy@hotmail.com>
2019-02-10 02:28:24 -0600

    adding crud functions with mongo orm

--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ const express = require('express'),
   
 
   //set configuration
+  const businessRoute = require('./routes/business.route');
   mongoose.Promise = global.Promise;
   mongoose.connect(config.DB, {useNewUrlParser: true}).then(
     () => {console.log('Database is connected')},
@@ -16,6 +17,7 @@ const express = require('express'),
   const app = express();
   app.use(bodyParser.json());
   app.use(cors());
+  app.use('/business', businessRoute);
   let port = process.env.PORT || 4000;
 
   const server = app.listen(function() {
