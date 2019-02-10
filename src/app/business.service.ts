833abecc0989ed23d23514c4a8f09a7d10f14c54
JarvisXxX <eldrizzy@hotmail.com>
2019-02-10 02:04:42 -0600

    Connecting to mongodb

--- src/app/business.service.ts
+++ src/app/business.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class BusinessService {
-  uri = 'http://localhost:4200/business';
+  uri = 'http://localhost:4000/business';
 
   constructor(private http: HttpClient) {
 
