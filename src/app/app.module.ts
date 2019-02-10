0e86ae0deb4e9b4cf597ba2677922269a8c1e49c
JarvisXxX <eldrizzy@hotmail.com>
2019-02-10 00:07:10 -0600

    adding the service to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GstAddComponent } from './gst-add/gst-add.component';
@@ -11,6 +12,7 @@ import { GstGetComponent } from './gst-get/gst-get.component';
 import { GstEditComponent } from './gst-edit/gst-edit.component';
 
 import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { BusinessService } from './business.service';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,7 @@ import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [BusinessService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
