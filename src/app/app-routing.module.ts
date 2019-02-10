3b65517250266989f6b93fcf7c88b71ba41ec12c
JarvisXxX <eldrizzy@hotmail.com>
2019-02-09 22:52:35 -0600

    Adding navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { GstGetComponent } from './gst-get/gst-get.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GstAddComponent } from './gst-add/gst-add.component';
+import { GstEditComponent } from './gst-edit/gst-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'busines/create',
+    component: GstAddComponent
+  },
+  {
+    path: 'business/edit/:id',
+    component: GstEditComponent
+  },
+  {
+    path: 'business',
+    component: GstGetComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
